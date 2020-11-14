4ede60b731f74c0df4146ccb67cd2f6a2f862e22
Tom Mitchell <tom.0505@hotmail.co.uk>
2020-11-14 14:58:51 +0000

    Calculate hydration correctly, send to API

--- sensor.py
+++ sensor.py
@@ -1,34 +1,32 @@
+"""sensor - measure soil hydration percentage"""
+
 import logging
+import os
 import sys
 import time
 
 import board
 import busio
+import requests
 from adafruit_ads1x15 import ads1115, analog_in
 
-MAX_VOLTAGE = 3.3
 MIN_VOLTAGE = 0.0
+MAX_VOLTAGE = 3.3
 
-logging.basicConfig(
-    stream=sys.stdout,
-    level=logging.INFO,
-    format="[%(asctime)s] [%(levelname)s] %(message)s",
-    datefmt="%Y-%m-%d %H:%M:%S",
-)
-logger = logging.getLogger(__name__)
+# Voltages read by the probbe in given environments
+VOLTAGE_WHEN_WATER = 1.2
+VOLTAGE_WHEN_AIR = 3.2
 
 
-def clamp_voltage(voltage: float) -> float:
-    """It's possible we read a voltage higher than we're supplying.
-    Clamp to a maximum of 3.3v to make calculations easier.
-    """
-    if voltage < MIN_VOLTAGE:
-        return MIN_VOLTAGE
+def clamp(value: float, min_value: float, max_value: float) -> float:
+    """Clamp a value between a minimum and maximum"""
+    if value < min_value:
+        return min_value
 
-    if voltage > MAX_VOLTAGE:
-        return MAX_VOLTAGE
+    if value > max_value:
+        return max_value
 
-    return voltage
+    return value
 
 
 def normalise(value, max_value: float, min_value: float) -> float:
@@ -37,17 +35,39 @@ def normalise(value, max_value: float, min_value: float) -> float:
 
 
 if __name__ == "__main__":
+    logging.basicConfig(
+        stream=sys.stdout,
+        level=logging.INFO,
+        format="[%(asctime)s] [%(levelname)s] %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+    )
+
+    api_token = os.environ["CACTUS_API_TOKEN"]
+    api_endpoint = os.environ["CACTUS_API_ENDPOINT"]
+
     i2c_bus = busio.I2C(board.SCL, board.SDA)
     adc = ads1115.ADS1115(i2c_bus)
     channel = analog_in.AnalogIn(adc, ads1115.P0)
 
     while True:
+        # Read the raw voltage from the sensor and ensure it's in a range
+        # we expected. The adc can report more tha 3.3v, despite that
+        # being all the voltage it's supplied with... Magic!
+        clamped_voltage = clamp(channel.voltage, MIN_VOLTAGE, MAX_VOLTAGE)
+
+        # The max voltage voltage I read with the sensor in open air was about
+        # VOLTAGE_WHEN_AIR, and VOLTAGE_WHEN_WATER when fully submerged in water.
+        # We interpolate a value from the voltage we read that's somewhere
+        # between these two, as will be the case with damp soil - not air, nor water.
         percentage_hydration = normalise(
-            value=clamp_voltage(channel.voltage),
-            # Pass these backwards - open ciruit represents no moisture
-            max_value=MIN_VOLTAGE,
-            min_value=MAX_VOLTAGE,
+            clamped_voltage, VOLTAGE_WHEN_WATER, VOLTAGE_WHEN_AIR
         )
 
-        logger.info("%.1f", percentage_hydration)
+        requests.post(
+            api_endpoint,
+            json={"hydration": percentage_hydration},
+            headers={"X-Cactus-Auth": api_token},
+        )
+        logging.info("Sent a hydration of %.1f to the API", percentage_hydration)
+
         time.sleep(1)
